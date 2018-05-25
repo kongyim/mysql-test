2815bed207a2436e61bf5ab4465e61d7e2687466
Kong Yim <kong.yim@imagination.com>
2018-05-25 14:07:24 +0800

    ADD: add remove update delet api

--- ApiManager.js
+++ ApiManager.js
@@ -4,6 +4,8 @@ var log4js = require('log4js');
 var _ = require('lodash');
 var path = require('path');
 var Q = require('q');
+var squel = require("squel");
+var bodyParser = require('body-parser')
 
 class ApiManager {
 
@@ -25,10 +27,13 @@ class ApiManager {
     let schema = {}
     Q()
       .then(()=>{
-        let sql = `select * from information_schema.tables where table_type = 'BASE TABLE'`
+        let sql = squel.select()
+                    .from("information_schema.tables")
+                    .where("table_type = 'BASE TABLE'")
+                    .toString()
         return Q.ninvoke(req.connection, "query", sql);
       })
-      .spread((result)=>{
+      .spread(result=>{
         return Q.all(_.map(result, (item)=>{
           let sql = `desc ${item.TABLE_NAME}`
           return Q.ninvoke(req.connection, "query", sql).spread(list => schema[item.TABLE_NAME] = list );
@@ -48,11 +53,12 @@ class ApiManager {
   getResource(req, res, next) {
     Q()
       .then(()=>{
-        let sql = `select * from ${req.params.resource}`
+        let sql = squel.select()
+                    .from(req.params.resource)
+                    .toString()
         return Q.ninvoke(req.connection, "query", sql);
       })
-      .spread((result)=>{
-        if (error) return res.status(500).send(error);
+      .spread(result=>{
         if (req.query) {
           result = _.filter(result, req.query )
         }
@@ -69,12 +75,90 @@ class ApiManager {
       .then(()=>{
         req.connection.end();
       })
+  }
 
+  createResource(req, res, next) {
+    Q()
+      .then(()=>{
+        let sql = squel.insert()
+                    .into(req.params.resource)
+                    .setFields(req.body)
+                    .toString()
+        return Q.ninvoke(req.connection, "query", sql);
+      })
+      .spread(result=>{
+        if (!result) return res.sendStatus(404)
+        req.body.id = result.insertId;
+        res.send(req.body)
+      })
+      .catch(error=> {
+        return res.status(500).send(error);
+      })
+      .then(()=>{
+        req.connection.end();
+      })
+  }
+
+  updateResource(req, res, next) {
+    Q()
+      .then(()=>{
+        let sql = squel.update()
+                    .table(req.params.resource)
+                    .setFields(req.body)
+                    .where(`id = ${req.params.id}`)
+                    .toString()
+        return Q.ninvoke(req.connection, "query", sql);
+      })
+      .spread(result=>{
+        if (!result || !result.affectedRows) return res.sendStatus(404)
+      })
+      .then(()=>{
+        let sql = squel.select()
+                    .from(req.params.resource)
+                    .where(`id = ${req.params.id}`)
+                    .toString()
+        return Q.ninvoke(req.connection, "query", sql);
+      })
+      .spread(result=>{
+        if (!result) return res.sendStatus(404)
+        res.send(_.head(result));
+      })
+      .catch(error=> {
+        return res.status(500).send(error);
+      })
+      .then(()=>{
+        req.connection.end();
+      })
+  }
+
+  removeResource(req, res, next) {
+    Q()
+      .then(()=>{
+        let sql = squel.delete()
+                    .from(req.params.resource)
+                    .where(`id = ${req.params.id}`)
+                    .toString()
+        return Q.ninvoke(req.connection, "query", sql);
+      })
+      .spread(result=>{
+        if (!result || !result.affectedRows) return res.sendStatus(404)
+        res.sendStatus(200)
+      })
+      .catch(error=> {
+        return res.status(500).send(error);
+      })
+      .then(()=>{
+        req.connection.end();
+      })
   }
 
   initRoutes() {
+    this.app.use(bodyParser.json())
     this.app.get(`/`, this.createConnection.bind(this), this.getResourceList.bind(this) )
     this.app.get(`/:resource/:id?`, this.createConnection.bind(this), this.getResource.bind(this) )
+    this.app.post(`/:resource`, this.createConnection.bind(this), this.createResource.bind(this) )
+    this.app.delete(`/:resource/:id?`, this.createConnection.bind(this), this.removeResource.bind(this) )
+    this.app.put(`/:resource/:id`, this.createConnection.bind(this), this.updateResource.bind(this) )
   }
 }
 
